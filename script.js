22b17ed996f62ef93fa42e50a6edfb0bb03df878
Shibnath Hansda <196810131+Hero24-x@users.noreply.github.com>
2025-11-20 15:27:30 +0530

    Remove fade-in effect on scroll

--- script.js
+++ script.js
@@ -1,14 +1,1 @@
-// Fade-in effect on scroll
-const elements = document.querySelectorAll(".fade");
 
-function fadeInOnScroll() {
-    elements.forEach(el => {
-        const rect = el.getBoundingClientRect();
-        if (rect.top < window.innerHeight - 100) {
-            el.classList.add("show");
-        }
-    });
-}
-
-window.addEventListener("scroll", fadeInOnScroll);
-fadeInOnScroll();
